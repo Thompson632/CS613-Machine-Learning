002efd80c32560425d7eadce90747be11cedf994
Robert Thompson <RobertThompson@Roberts-MBP.home>
2022-05-14 17:08:39 -0400

    PCA is cleaner

--- hw4/src/knn.py
+++ hw4/src/knn.py
@@ -13,23 +13,23 @@ class KNN:
         self.y_train = y
     
     def evaluate_model(self, X):
-        class_preds = []
-        
-        for x in X:
-            class_to_assign = self.evaluate(x)
-            class_preds.append(class_to_assign)
-            
-        return np.array(class_preds)
+        y_pred = [self.evaluate(x) for x in X]
+        return np.array(y_pred)
     
     def evaluate(self, x):
+        # Compute distances between x and all examples in the training set
         distances = [self.calculate_euclidean_distance(x, x_train) for x_train in self.X_train]
+        # Sort by distance and return indices of the first k neighbors
         k_idx = np.argsort(distances)[: self.k]
+        # Extract the labels of the k nearest neighbor training samples
         k_neighbor_labels = [self.y_train[i] for i in k_idx]
+        # return the most common class label
         most_common = Counter(k_neighbor_labels).most_common(1)
         return most_common[0][0]
     
     def calculate_euclidean_distance(self, x1, x2):    
-        square_val = np.square(x1 - x2)
-        sum_val = np.sum(square_val)
-        distance = np.sqrt(sum_val)
-        return distance
+        # square_val = np.square(x1 - x2)
+        # sum_val = np.sum(square_val)
+        # distance = np.sqrt(sum_val)
+        # return distance
+        return np.sqrt(np.sum((x1 - x2) ** 2))
